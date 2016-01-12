f29452d88066d5c4e790785ca16f7721f5fa752a
yotam nordman <yotam nordman>
2016-01-12 12:31:41 +0200

    managed to fix clicksave using a file and threading

--- modules/click_save.py
+++ modules/click_save.py
@@ -1,11 +1,26 @@
 import pythoncom,pyHook
 from datetime import datetime
+import os
+import threading
 def run(**args):
-    click_list=[]
+    fo = open("output.txt", "w")
+    fo.close()
+    t = threading.Thread(target=clicks,)
+    t.start()
+    while (os.stat("output.txt").st_size == 0):
+        pass
+    fi = open("output.txt", "r+")
+    data = fi.read()
+    fi.close()
+    return data
+def clicks(**args):
+    click_list = []
     def onclick(event):
-        if(len(click_list) > 10):
+        if(len(click_list) == 10):
             hm.UnhookMouse()
-            return click_list
+            fo = open("output.txt", "w")
+            fo.write(str(click_list))
+            fo.close()
         click_timer = str(datetime.now())
         x = (event.Position,click_timer)
         click_list.append(x)
