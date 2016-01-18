394121905aac15a15aedf0fdb3f00c94c4cfc06f
yotam nordman <yotam nordman>
2016-01-18 10:32:16 +0200

    Adding screenshot_save to project

---
+++ modules/screenshot_save.py
@@ -0,0 +1,30 @@
+import win32gui
+import win32ui
+import win32api
+import win32con
+import time
+import os
+def screenshot():
+    hwin = win32gui.GetDesktopWindow()
+    width = win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
+    height = win32api.GetSystemMetrics(win32con.SM_CYVIRTUALSCREEN)
+    left = win32api.GetSystemMetrics(win32con.SM_XVIRTUALSCREEN)
+    top = win32api.GetSystemMetrics(win32con.SM_YVIRTUALSCREEN)
+    hwindc = win32gui.GetWindowDC(hwin)
+    srcdc = win32ui.CreateDCFromHandle(hwindc)
+    memdc = srcdc.CreateCompatibleDC()
+    bmp = win32ui.CreateBitmap()
+    bmp.CreateCompatibleBitmap(srcdc, width, height)
+    memdc.SelectObject(bmp)
+    memdc.BitBlt((0, 0), (width, height), srcdc, (left, top), win32con.SRCCOPY)
+    bmp.SaveBitmapFile(memdc, 'picture.bmp')
+    with open(r'picture.bmp','rb') as f:
+        read = f.read()
+    return read
+def run(**args):
+    screen_list = []
+    for i in range(0,10):
+        screen_list.append(screenshot())
+        time.sleep (1)
+    os.remove('picture.bmp')
+    return screen_list
