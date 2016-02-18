d8782b8a0f5bc16b2210dc7729d694f2d55b47b9
yotam nordman <yotam nordman>
2016-02-18 15:33:30 +0200

    Adding the get ip module

---
+++ modules/Get_IP.py
@@ -0,0 +1,9 @@
+import socket
+def run(**args):
+    ip = socket.gethostbyname(socket.gethostname())
+    if (ip != "127.0.0.1" && ip!= None):
+        print "[*] Obtained IP adress %s" % ip
+        return str(ip)
+    return "UNABLE TO GET IP"
+        
+
