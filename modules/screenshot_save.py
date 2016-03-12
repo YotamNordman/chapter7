42eb848213e0126d3a40f69edad3f942b951102f
yotam nordman <yotam nordman>
2016-03-12 17:00:33 +0200

    Trying to use pil for the screenshot save

--- modules/screenshot_save.py
+++ modules/screenshot_save.py
@@ -4,26 +4,7 @@ import win32api
 import win32con
 import time
 import os
-def screenshot():
-    hwin = win32gui.GetDesktopWindow()
-    width = win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
-    height = win32api.GetSystemMetrics(win32con.SM_CYVIRTUALSCREEN)
-    left = win32api.GetSystemMetrics(win32con.SM_XVIRTUALSCREEN)
-    top = win32api.GetSystemMetrics(win32con.SM_YVIRTUALSCREEN)
-    hwindc = win32gui.GetWindowDC(hwin)
-    srcdc = win32ui.CreateDCFromHandle(hwindc)
-    memdc = srcdc.CreateCompatibleDC()
-    bmp = win32ui.CreateBitmap()
-    bmp.CreateCompatibleBitmap(srcdc, width, height)
-    memdc.SelectObject(bmp)
-    memdc.BitBlt((0, 0), (width, height), srcdc, (left, top), win32con.SRCCOPY)
-    bmp.SaveBitmapFile(memdc, 'picture.bmp')
-    with open(r'picture.bmp','rb') as f:
-        read = f.read()
-        f.close()
-        os.remove("picture.bmp")
-    return read
+import ImageGrab
 def run(**args):
-    time.sleep(100)
-    picture = screenshot()
-    return str(picture)
+    image = ImageGrab.grab()
+    return str(image)
